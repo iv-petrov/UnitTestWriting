47a58da8cc8d064b5718325e3322a4de3ef021e4
Петров Игорь <igvapetrov@inno.tech>
2025-05-01 21:50:01 +0300

    Первый тест

--- UnitTestWriting.Tests/Domain/CartTests.cs
+++ UnitTestWriting.Tests/Domain/CartTests.cs
@@ -1,3 +1,4 @@
+using UnitTestWriting.Domain;
 using Xunit;
 
 namespace UnitTestWriting.Tests.Domain
@@ -5,14 +6,16 @@ namespace UnitTestWriting.Tests.Domain
     public class CartTests
     {
         [Fact]
-        public void Test()
+        public void Ctor_SingleProduct_ReturnFullPrice()
         {
             // Arrange
-
+            var product = new Product { Price = 1000 };
+            var user = new User();
             // Act
-
+            var cart = new Cart(user);
+            cart.AddProduct(product, 20);
             // Assert
-            Assert.True(true);
+            Assert.Equal(20000, cart.GetFullPrice(DateTime.Now));
         }
     }
 }
